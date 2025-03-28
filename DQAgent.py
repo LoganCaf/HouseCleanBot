68a9d050afff5414afec769fb4c4d176e90358ab
LoganCaf <logancaf96@gmail.com>
2025-03-28 17:35:07 -0600

    basic class

--- DQAgent.py
+++ DQAgent.py
@@ -1,3 +1,20 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from collections import deque
+import random
+
 class DQAgent:
     def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def buildModel(self):
+        pass
+
+    def act(self):
         pass
